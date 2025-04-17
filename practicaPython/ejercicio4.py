81bb8d17e2e737688ce115e74a718ab061fb6b50
Andres7266 <andres.alicalcina@intjem.edu.bo>
2025-04-17 16:27:04 -0400

    Update ejercicio4.py

--- practicaPython/ejercicio4.py
+++ practicaPython/ejercicio4.py
@@ -1,3 +1,4 @@
+"""De la siguiente lista de números: 10, 20, 45,45, 7, 90, 88, 45,910. Mostrar cuántas veces se repite el 45 y cuantas veces se repite el 90. Usar for"""
 lista = [10, 20, 45, 45, 7, 90, 88, 45, 910]
 repetidos = [num for num in set(lista)
               if lista.count(num) > 1]
